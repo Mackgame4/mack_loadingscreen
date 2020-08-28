3946f79021ad238b38bbecf6e8836c7ae656c0e0
Mackgame4 <42896437+Mackgame4@users.noreply.github.com>
2020-08-28 19:40:07 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+games { 'rdr3', 'gta5' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'Mackgame4&KingRich'
+description 'Just a simple rework by Mackgame4 of TFRP Loadingscreen by KingRich inspired on 3QRP Loadingscreen (Portugal/Italy)'
+version '1.0.0'
+
+files {
+    'index.html',
+    'style.css',
+	'index.js',
+    'logo.png',
+    'background.jpg',
+    'music/music.mp3'
+}
+
+loadscreen 'index.html'
